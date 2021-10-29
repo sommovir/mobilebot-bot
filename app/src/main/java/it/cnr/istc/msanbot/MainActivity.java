1eed16d36bd1d2bc3b2da50ad1c69cbf4e4f96df
Alessio Pecilli <alessiopecilli@gmail.com>
2021-10-29 17:06:19 +0200

    Added speechManager and systemManager

--- app/src/main/java/it/cnr/istc/msanbot/MainActivity.java
+++ app/src/main/java/it/cnr/istc/msanbot/MainActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.sanbot.opensdk.base.TopBaseActivity;
+import com.sanbot.opensdk.beans.FuncConstant;
+import com.sanbot.opensdk.beans.OperationResult;
+import com.sanbot.opensdk.function.beans.EmotionsType;
+import com.sanbot.opensdk.function.beans.SpeakOption;
+import com.sanbot.opensdk.function.unit.SpeechManager;
+import com.sanbot.opensdk.function.unit.SystemManager;
 
 public class MainActivity extends TopBaseActivity {
+
+    SpeechManager speechManager = (SpeechManager)getUnitManager(FuncConstant. SPEECH_MANAGER);
+    SystemManager systemManager= (SystemManager)getUnitManager(FuncConstant. SYSTEM_MANAGER);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,25 @@ public class MainActivity extends TopBaseActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button;
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speechManager.startSpeak("Se il sole bacia i belli");
+                systemManager.showEmotion(EmotionsType.SMILE);
+            }
+        });
     }
 
     @Override
     protected void onMainServiceConnected() {
-        System.out.println("sono qui");
-        Toast.makeText(this,"ciao",Toast.LENGTH_LONG).show();
+        speechManager.startSpeak("Sono connesso");
     }
+
+
+
 }
