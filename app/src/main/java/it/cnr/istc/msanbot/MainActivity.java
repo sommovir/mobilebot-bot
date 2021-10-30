f48d6cfae9ab30a057f0b3d5d9a8d4f9d735df12
Alessio Pecilli <alessiopecilli@gmail.com>
2021-10-30 14:46:41 +0200

    #2 commit

--- app/src/main/java/it/cnr/istc/msanbot/MainActivity.java
+++ app/src/main/java/it/cnr/istc/msanbot/MainActivity.java
@@ -1,12 +1,15 @@
 package it.cnr.istc.msanbot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sanbot.opensdk.base.TopBaseActivity;
@@ -14,13 +17,24 @@ import com.sanbot.opensdk.beans.FuncConstant;
 import com.sanbot.opensdk.beans.OperationResult;
 import com.sanbot.opensdk.function.beans.EmotionsType;
 import com.sanbot.opensdk.function.beans.SpeakOption;
+import com.sanbot.opensdk.function.beans.StreamOption;
+import com.sanbot.opensdk.function.beans.speech.Grammar;
+import com.sanbot.opensdk.function.beans.speech.RecognizeTextBean;
+import com.sanbot.opensdk.function.beans.speech.SpeakStatus;
+import com.sanbot.opensdk.function.unit.HDCameraManager;
+import com.sanbot.opensdk.function.unit.MediaManager;
 import com.sanbot.opensdk.function.unit.SpeechManager;
 import com.sanbot.opensdk.function.unit.SystemManager;
+import com.sanbot.opensdk.function.unit.interfaces.media.MediaListener;
+import com.sanbot.opensdk.function.unit.interfaces.media.MediaStreamListener;
+import com.sanbot.opensdk.function.unit.interfaces.speech.RecognizeListener;
+import com.sanbot.opensdk.function.unit.interfaces.speech.SpeakListener;
 
-public class MainActivity extends TopBaseActivity {
+public class MainActivity extends TopBaseActivity implements MediaListener{
 
     SpeechManager speechManager = (SpeechManager)getUnitManager(FuncConstant. SPEECH_MANAGER);
     SystemManager systemManager= (SystemManager)getUnitManager(FuncConstant. SYSTEM_MANAGER);
+    HDCameraManager hdCameraManager = (HDCameraManager)getUnitManager(FuncConstant.HDCAMERA_MANAGER);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +43,83 @@ public class MainActivity extends TopBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        hdCameraManager.setMediaListener(new MediaStreamListener() {
+            @Override
+            public void getVideoStream(int i, byte[] bytes, int i1, int i2) {
+
+            }
+
+            @Override
+            public void getAudioStream(int i, byte[] bytes) {
+
+            }
+        });
+
         Button button;
 
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.button_mainButton_speak);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                speechManager.startSpeak("Se il sole bacia i belli");
-                systemManager.showEmotion(EmotionsType.SMILE);
+                //speechManager.startSpeak("Uga Buga Uga Tunga");
+                //systemManager.showEmotion(EmotionsType.SMILE);
+                speechManager.doWakeUp();
+
+
             }
         });
+
+        speechManager.setOnSpeechListener(new RecognizeListener() {
+            @Override
+            public boolean onRecognizeResult(Grammar grammar) {
+                String x = grammar.getText();
+                Toast.makeText(MainActivity.this, x, Toast.LENGTH_SHORT).show();
+                speechManager.startSpeak("hai detto" + x);
+                if (x.equals("ciao")) {
+                    speechManager.startSpeak("Mi hai salutato");
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onRecognizeText(RecognizeTextBean recognizeTextBean) {
+                speechManager.startSpeak("onRecognizeText");
+                Toast.makeText(MainActivity.this, recognizeTextBean.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRecognizeVolume(int i) {
+                speechManager.startSpeak("onRecognizeVolume" + i);
+                Toast.makeText(MainActivity.this, "Volume" + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartRecognize() {
+                speechManager.startSpeak("onStartRecognize");
+                Toast.makeText(MainActivity.this, "Inizio speak", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopRecognize() {
+                speechManager.startSpeak("onStopRecognize");
+                Toast.makeText(MainActivity.this, "Fine speak", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(int i, int i1) {
+                speechManager.startSpeak("onStopRecognize");
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     @Override
     protected void onMainServiceConnected() {
         speechManager.startSpeak("Sono connesso");
     }
-
-
 
 }
